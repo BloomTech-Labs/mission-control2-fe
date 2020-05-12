5d3acdacc12ed7f4a6da97120db4921c10bbb827
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-05-12 15:50:46 -0600

    added usability

---
+++ src/components/login/AppWithRouterAccess.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, useHistory } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import Home from './Home';
+import Login from './Login';
+import Protected from './Protected';
+
+const AppWithRouterAccess = () => {
+    const history = useHistory();
+    const onAuthRequired = () => {
+        history.push('/login');
+    };
+    //creates a secure route at localhost, along with a client id. subject to change
+    //if it works, pushes you to implicit callbal
+    //makes it required to have authorization
+    //then routing. could be condensed if needed and just shoved in app.js TBH.
+    return (
+        <Security issuer='https://localhost:3000/oauth2/default'
+            clientId='0oabxhxxeiHbyU0C54x6'
+            redirectUri={window.location.origin + '/implicit/callback'}
+            onAuthRequired={onAuthRequired}
+            pkce={true} >
+            <Route path='/' exact={true} component={Home} />
+            <SecureRoute path='/protected' component={Protected} />
+            <Route path='/login' render={() => <Login issuer='https://localhost:3000/oauth2/default' />} />
+            <Route path='/implicit/callback' component={LoginCallback} />
+        </Security>
+    );
+};
+export default AppWithRouterAccess;
---
+++ src/components/login/Protected.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+//what we will be linking to. will be actual app.
+export default () => <h3>Protected</h3>;
