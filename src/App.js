211e062dcd4b63551c49e7c94ec306cfe63cb2a8
apple <apple@apples-MacBook-Pro-2.local>
2020-05-12 22:37:26 -0700

    replaced code to work as intended, removed my person work around to display my components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import AppWithRouterAccess from './components/login/AppWithRouterAccess';
-import Dash from './components/Dashboard/Dash';
+import AppWithRouterAccess from './components/login/AppWithRouterAccess';
+// import Dash from './components/Dashboard/Dash';
 const App = () => {
   return (
     <Router>
-      {/* <AppWithRouterAccess /> */}
-      <Dash/>
+      <AppWithRouterAccess />
     </Router>
   );
 }
