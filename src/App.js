14e22b42a2805582a1e309ec552cca3289df26e4
apple <apple@apples-MacBook-Pro-2.local>
2020-05-13 21:45:44 -0700

    made adjustments to launch app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import AppWithRouterAccess from './components/login/AppWithRouterAccess';
-import Dash from './components/Dashboard/Dash';
+import AppWithRouterAccess from './components/login/AppWithRouterAccess';
+// import Dash from './components/Dashboard/Dash';
 import theme from './components/MaterialUI/theme';
 import { ThemeProvider } from '@material-ui/core/styles';
 
@@ -9,8 +9,7 @@ const App = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        {/* <AppWithRouterAccess /> */}
-        <Dash/>
+        <AppWithRouterAccess />
       </ThemeProvider>
     </Router>
   );
